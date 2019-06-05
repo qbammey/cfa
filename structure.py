9692222540d8c30436e0189d3e008a037768aaee
Quentin Bammey <quentin.bammey@ens-lyon.fr>
2019-06-05 19:08:42 +0200

    First commit

---
+++ structure.py
@@ -0,0 +1,12 @@
+"""This file contains the structure of cfa detection methods."""
+
+
+__author__ = "Quentin Bammey" 
+__credits__ = ["Quentin Bammey", "Rafael Grompone", "Jean-Michel Morel"]
+__license__ = "GPL"
+__version__ = "0.0.0"
+__maintainer__ = "Quentin Bammey"
+__email__ = "quentin.bammey@cmla.ens-cachan.fr"
+__status__ = "Development"
+
+
